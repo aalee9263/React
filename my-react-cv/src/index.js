acbcd7c217cbf626ea9715b2c2828ba78d16732d
aalee9263 <144584171+aalee9263@users.noreply.github.com>
2023-12-21 19:11:26 +0500

    initial

--- my-react-cv/src/index.js
+++ my-react-cv/src/index.js
@@ -1,8 +1,25 @@
 import react from 'react';
 import reactDOM from 'react-dom';
 
+let myNumber = 24
+ 
 function Hi (){
-  return <div>Hello World!</div>
+  return <div>Hello <strong>Arshad!</strong>
+  
+  
+  
+  </div>
 }
+
+          <div className='items'>abc items
+          <li>
+            <ul>items number 1</ul>
+            <ul>items number 2</ul>
+            <ul>items number 3</ul>
+            <ul>items number 4</ul>
+            <ul>items number {myNumber + 34}</ul>
+
+          </li>
+          </div>
 
 reactDOM.render(<Hi/>, document.querySelector('#root'));
